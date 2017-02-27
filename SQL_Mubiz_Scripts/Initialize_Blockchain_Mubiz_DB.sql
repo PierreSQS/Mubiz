e484a65ea4e30119714a8fbdbd82371241cf20bb
odilebaima <obaima@yahoo.fr>
2017-02-27 22:18:49 +0100

    added SQL Scripts

---
+++ SQL_Mubiz_Scripts/Initialize_Blockchain_Mubiz_DB.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS Blockchain_MubizDB;
+CREATE DATABASE Blockchain_MubizDB CHARACTER SET 'utf8';
+
+use Blockchain_MubizDB;
+SET NAMES utf8;
+
+CREATE TABLE Block (
+   hash VARCHAR(80)  PRIMARY KEY, 
+   confirmations VARCHAR(40),
+   strippedsize INT,
+   size VARCHAR(40),
+   weight VARCHAR(40),
+   height INT,
+   version VARCHAR(40),
+   versionHex VARCHAR(40),
+   merkleroot VARCHAR(80),
+   -- tx VARCHAR(5000),	
+   time VARCHAR(40),
+   mediantime VARCHAR(40),
+   nonce VARCHAR(40),
+   bits VARCHAR(40),
+   difficulty VARCHAR(40),
+   chainwork VARCHAR(80),
+   previousblockhash VARCHAR(80),
+   nextblockhash VARCHAR(80)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+describe block;
