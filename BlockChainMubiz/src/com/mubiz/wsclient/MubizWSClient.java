d71f4ca9e19c9fa97fa77d7dc875b821e18197f9
Pierre Armand Nomo Messina <Nomop@de.group.intl>
2017-03-03 14:56:07 +0100

    permuted code from MubizWSTestClient <-> MubizWSClient for coherence

--- BlockChainMubiz/src/com/mubiz/wsclient/MubizWSClient.java
+++ BlockChainMubiz/src/com/mubiz/wsclient/MubizWSClient.java
@@ -1,30 +1,72 @@
 package com.mubiz.wsclient;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import org.apache.http.client.ClientProtocolException;
 
 import com.mubiz.httpclient.MubizHttpClient;
 import com.mubiz.jsonparser.MubizJsonParser;
 import com.mubiz.dao.Block;
+import com.mubiz.dao.Hash;
+import com.mubiz.dao.NumberOfBlocks;
+import com.mubiz.database.BlockTable;
+import com.mubiz.database.MySQLUtilities;
 
 public class MubizWSClient {
-	public static void main(String[] args) throws ClientProtocolException, IOException {
-		
+	public static void main(String[] args) throws ClientProtocolException, IOException, SQLException {
+
+		// Initializing the Http Client
 		MubizHttpClient myMubizHttpClient = new MubizHttpClient();
 
-		
-		String responseBody = myMubizHttpClient.connectToWS(MubizHttpClient.MUBIZ_BLOC_HASH_URL
-				+ "000000000000000000207a44417907a44175fc9152dc05b901d180a68fe42070/");
-		
+		// Initializing Database Connection
 
-		MubizJsonParser.deserializeRespFromJSON(responseBody, Block.class);
-		
-		
-		//Block block = MubizJsonParser.deserializeRespFromJSON(responseBody, Block.class);
-		// System.out.println("---------------- the Block
-		// ------------------------");
-		// System.out.println("the deserialized Block:"+block);
+		MySQLUtilities myMySQLUtilities = new MySQLUtilities("mysql-sample-properties.xml");
+		Connection myConnection = myMySQLUtilities.getConnection();
 
+		BlockTable myBlockTable = new BlockTable(myConnection, myMySQLUtilities.dbName, myMySQLUtilities.dbms);
+
+		// get total number of blocks or the highest index
+		String blocksResp = myMubizHttpClient.connectToWS(MubizHttpClient.MUBIZ_BLOCKS_URL);
+
+		NumberOfBlocks blocksCounter = MubizJsonParser.deserializeRespFromJSON(blocksResp, NumberOfBlocks.class);
+
+		// counts the actual block in the WS
+		long actualBlocksInWS = blocksCounter.getBlocks();
+
+		// for test purposes
+		// long actualBlocksInWS = 25;
+
+		// counts the actual block in the DB
+		long actualBlocksInDB = BlockTable.getNumberOfRecord(myConnection);
+
+		// for test purposes
+		// long actualBlocksInDB = 455540;
+
+		System.out.println("the number of blocks actually in the WS: " + actualBlocksInWS);
+		System.out.println("the number of blocks actually in the DB: " + actualBlocksInDB);
+
+		// just for test purposes
+		for (long i = 160; i > actualBlocksInDB; i--) {
+
+			// CAUTION!! the loop should only insert the blocks which are new to
+			// the
+			// DB!!!!
+//      this should be the reality			
+//      for (long i = actualBlocksInWS; i > actualBlocksInDB; i--) {
+
+			// get JSON Hash from WS
+			String hashResp = myMubizHttpClient.connectToWS(MubizHttpClient.MUBIZ_BLOCK_INDEX_URL + i + "/");
+			Hash hash = MubizJsonParser.deserializeRespFromJSON(hashResp, Hash.class);
+			System.out.println("the data of the Block (block#" + i + ")");
+
+			// get block data
+			String blockResp = myMubizHttpClient
+					.connectToWS(MubizHttpClient.MUBIZ_BLOC_HASH_URL + hash.getBlock_hash() + "/");
+			Block block = MubizJsonParser.deserializeRespFromJSON(blockResp, Block.class);
+			System.out.println("inserting block : block#" + i);
+			myBlockTable.insertRow(block);
+		}
 	}
 }
